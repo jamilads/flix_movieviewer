34dfdd7d1e8fc37448ead53edc62d8e445b9c814
Jamila Duke-Smith <jamilads@Jamilas-MacBook-Pro.local>
2018-09-19 17:07:29 -0400

    fixed

---
+++ Flix_MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix_MovieViewer
+//
+//  Created by Jamila Duke-Smith on 9/19/18.
+//  Copyright © 2018 Jamila Duke-Smith. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
